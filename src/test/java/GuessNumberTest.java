d56120e4c28ef13ebbb944fdd91b67aa12503225
ygq <493650029@qq.com>
2020-06-19 17:03:17 +0800

    [ygq] should_return_0A4B_when_comparewith_4321_given_1234()

--- src/test/java/GuessNumberTest.java
+++ src/test/java/GuessNumberTest.java
@@ -19,4 +19,16 @@ public class GuessNumberTest {
         //then
         assertEquals("4A0B",result);
     }
+
+    @Test
+    public void should_return_0A4B_when_comparewith_4321_given_1234(){
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        List<String> gameAnswer = Arrays.asList("1","2","3","4");
+        List<String> userAnswer = Arrays.asList("4","3","2","1");
+        //when
+        String result = guessNumber.answer(gameAnswer,userAnswer);
+        //then
+        assertEquals("0A4B",result);
+    }
 }
