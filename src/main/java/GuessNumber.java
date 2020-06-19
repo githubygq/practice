59efa14db515f89ccd300bbdf6e18d670c2757d6
ygq <493650029@qq.com>
2020-06-19 16:33:49 +0800

    [ygq] should_return_4A0B_given_1234

---
+++ src/main/java/GuessNumber.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GuessNumber {
+
+    public String answer(List<Integer> gameAnswer, List<Integer> userAnswer) {
+        return "4A0B";
+    }
+}
---
+++ src/test/java/GuessNumberTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class GuessNumberTest {
+
+    @Test
+    public void should_return_4A0B_given_1234(){
+        //given
+        GuessNumber guessNumber = new GuessNumber();
+        List<Integer> gameAnswer = Arrays.asList(1,2,3,4);
+        List<Integer> userAnswer = Arrays.asList(1,2,3,4);
+        //when
+        String result = guessNumber.answer(gameAnswer,userAnswer);
+        //then
+        assertEquals("4A0B",result);
+    }
+}
